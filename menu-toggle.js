4ca7c05f1ec9a59fb0b292aacda7cdbd24c33557
Jhonny Bueno <jhonnyrbueno@gmail.com>
2022-05-08 14:25:24 -0300

    commits

--- menu-toggle.js
+++ menu-toggle.js
@@ -11,33 +11,3 @@ menuToggle.addEventListener("click", () => {
     
     show = !show;
 })
-
-const $menuSection = document.querySelector('.menu-section')
-	
-	const $links1 = document.querySelector('.links1')
-	const $links2 = document.querySelector('.links2')
-	const $links3 = document.querySelector('.links3')
-	const $links4 = document.querySelector('.links4')
-    const $links5 = document.querySelector('.links5')
-
-	const $links_Open = document.querySelector('.links_Open')
-
-	$links1.addEventListener('click',function() {
-		$menu.classList.remove('in')
-	})
-
-	$links2.addEventListener('click',function() {
-		$menu.classList.remove('in')
-	})
-
-	$links3.addEventListener('click',function() {
-		$menu.classList.remove('in')
-	})
-
-	$links4.addEventListener('click',function() {
-		$menu.classList.remove('in')
-	})
-
-    $links5.addEventListener('click',function() {
-		$menu.classList.remove('in')
-	})
