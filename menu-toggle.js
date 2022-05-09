5ef0090d463b9bc0a04029818b6ec964dac320a3
Jhonny Bueno <jhonnyrbueno@gmail.com>
2022-05-08 21:12:52 -0300

    commits

--- menu-toggle.js
+++ menu-toggle.js
@@ -2,6 +2,8 @@ let show = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const menu = document.querySelector(".menu")
+const link = menu.querySelector("#link")
 
 menuToggle.addEventListener("click", () => {
 
@@ -10,4 +12,14 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     
     show = !show;
+})
+
+link.addEventListener("click", () => {
+
+    document.body.style.overflow = !show ? "hidden" : "initial"
+
+    menu.classList.toggle
+
+    show = !show
+    
 })
